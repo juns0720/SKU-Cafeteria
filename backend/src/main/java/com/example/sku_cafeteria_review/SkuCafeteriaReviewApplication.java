7f6e4ab91e94921778336eb7bad0f04c193ffd4a
junseo Lee <juns0720@naver.com>
2026-04-16 22:41:30 +0900

    chore: 프로젝트 초기 구조 셋업

---
+++ backend/src/main/java/com/example/sku_cafeteria_review/SkuCafeteriaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.sku_cafeteria_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuCafeteriaReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkuCafeteriaReviewApplication.class, args);
+	}
+
+}
